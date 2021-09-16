2837df319e73cf01f3abf0c29e787a03f466f919
bigfish_tx <yuxinggang@tongxingschool.com>
2021-09-16 22:35:42 +0800

    0.0.1

---
+++ TestView.framework/Headers/TestView.h
@@ -0,0 +1,19 @@
+//
+//  TestView.h
+//  TestView
+//
+//  Created by DaYu on 2021/9/16.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TestView.
+FOUNDATION_EXPORT double TestViewVersionNumber;
+
+//! Project version string for TestView.
+FOUNDATION_EXPORT const unsigned char TestViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestView/PublicHeader.h>
+
+
+#import <TestView/UITestView.h>
---
+++ TestView.framework/Headers/UITestView.h
@@ -0,0 +1,16 @@
+//
+//  UITestView.h
+//  TestView
+//
+//  Created by DaYu on 2021/9/16.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UITestView : UIView
+
+@end
+
+NS_ASSUME_NONNULL_END
